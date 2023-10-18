0a47469b875f6349ac2e2ca1afe4d0d754f01549
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-10-18 18:20:00 +0530

    Tutorial 8

---
+++ app/src/main/java/com/example/tutorial8/FragmentViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.tutorial8
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FragmentViewModel:ViewModel() {
+
+    private val backgroundColor=MutableLiveData<Int>()
+    fun getBackgroundColor():LiveData <Int>{
+        return backgroundColor
+
+    }
+
+    fun setBackgroundColor(color:Int){
+    backgroundColor.value=color
+    }
+}
---
+++ app/src/main/java/com/example/tutorial8/SettingsFragment.kt
@@ -0,0 +1,79 @@
+package com.example.tutorial8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Spinner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView=inflater.inflate(R.layout.fragment_settings,container,false)
+        // Inflate the layout for this fragment
+        val spinner:Spinner=rootView.findViewById(R.id.spinner2)
+        val saveButton:Button=rootView.findViewById(R.id.button4)
+
+        val ViewModel=ViewModelProvider(requireActivity())[FragmentViewModel::class.java]
+        saveButton.setOnClickListener{
+            when(spinner.selectedItem){
+                "blank"->{}
+                "Background 1"->{ViewModel.setBackgroundColor(R.color.background1)}
+                "Background 2"->{ViewModel.setBackgroundColor(R.color.background2)}
+                "Background 3"->{ViewModel.setBackgroundColor(R.color.background3)}
+                "Background 4"->{ViewModel.setBackgroundColor(R.color.background4)}
+                "Background 5"->{ViewModel.setBackgroundColor(R.color.background5)}
+
+            }
+        }
+        return rootView
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SettingsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SettingsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
